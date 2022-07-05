519c0f44bb182d5b299115e3e035691836250d69
molcathy <moldoveanu.catherine@gmail.com>
2022-07-05 15:16:58 +0100

    both versions completed

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,12 +1,11 @@
 import time, datetime
+from datetime import timedelta
 
 
 def add(dt):
-    now = dt.timestamp()
-    now_plus_gs = now + (10**9)
-    now_plus_gs_date = datetime.datetime.fromtimestamp(now_plus_gs)
-    return now_plus_gs_date
+    return dt + datetime.timedelta(seconds=10**9)
 
 
 dt = datetime.datetime(2021, 3, 5, 14, 30, 21)
 print(add(dt))
+https://www.geeksforgeeks.org/python-datetime-timedelta-function/
